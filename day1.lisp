eb1720144cb2879490e3ab5998b181a8191ee1fa
Ad <me@skissue.xyz>
2024-12-01 08:37:31 -0500

    day1: solve part 2

--- day1.lisp
+++ day1.lisp
@@ -9,3 +9,18 @@
         (right (sort (mapcar #'cadr data) #'<)))
     (reduce #'+ (mapcar #'abs (mapcar #'- left right)))))
 
+(defparameter *counts* (make-hash-table))
+
+(defun part2-generate-counts (data)
+  (setf *counts* (make-hash-table))
+  (let ((right (mapcar #'cadr data)))
+    (loop for x in right
+          for cur = (gethash x *counts* 0)
+          do
+             (setf (gethash x *counts*) (1+ cur)))))
+
+(defun part2 (data)
+  (part2-generate-counts data)
+  (reduce #'+
+          (mapcar (lambda (x) (* x (gethash x *counts* 0)))
+                  (mapcar #'car data))))
