b58e2c0e4f72fa2ec746221904ecbe3479584ca7
Ad <me@skissue.xyz>
2024-12-02 13:37:58 -0500

    day2: solve part 2
    Tried to do it smartly, but turns out brute force is the way out 💪.

--- day2.lisp
+++ day2.lisp
@@ -9,13 +9,27 @@
 (defparameter *data* (process-data "input/day2.txt"))
 
 (defun valid-report (report)
-  (let ((comp (if (> (car report)
-                     (cadr report))
-                  #'> #'<)))
+  (let ((comp (comp report)))
     (loop for a in report
           for b in (cdr report)
           always (funcall comp a b)
           always (<= 1 (abs (- a b)) 3))))
 
 (defun part1 (data)
-  (length (remove-if-not  #'valid-report data)))
+  (length (remove-if-not #'valid-report data)))
+
+(defun comp (report)
+  (if (> (car report)
+         (cadr report))
+      #'> #'<))
+
+(defun remove-nth (n list)
+  (append (subseq list 0 n) (nthcdr (1+ n) list)))
+
+(defun valid-report-2 (report)
+  (or (valid-report report)
+      (loop for i from 0 to (length report)
+            thereis (valid-report (remove-nth i report)))))
+
+(defun part2 (data)
+  (length (remove-if-not #'valid-report-2 data)))
