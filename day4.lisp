e6472099a7909f8661cf7760c1a7eac5c63a1b8d
Ad <me@skissue.xyz>
2024-12-04 10:27:17 -0500

    day4: solve part 2
    This honestly might be the worst code I've ever written 💀.

--- day4.lisp
+++ day4.lisp
@@ -34,5 +34,30 @@
                                     data (cons x y) '(#\M #\A #\S) dir)
                                 sum 1))))
 
+(defconstant +diags+ '((1 . 1)
+                       (-1 . -1)
+                       (1 . -1)
+                       (-1 . 1)))
 
+(defun get-diag (data pos dir)
+  (destructuring-bind ((x . y) . (dx . dy)) (cons pos dir)
+    (let ((nx (+ x dx))
+          (ny (+ y dy)))
+      (unless (and (< -1 nx (length (aref data 0)))
+                   (< -1 ny (length data)))
+        (return-from get-diag))
+      (aref (aref data ny) nx))))
+
+(defun part2 (data)
+  (loop for y below (length data)
+        sum (loop for x below (length (aref data 0))
+                  when (and (equal #\A (aref (aref data y) x))
+                            (member (loop for diag in +diags+
+                                          collect (get-diag data (cons x y) diag))
+                                    '((#\M #\S #\M #\S)
+                                      (#\M #\S #\S #\M)
+                                      (#\S #\M #\M #\S)
+                                      (#\S #\M #\S #\M))
+                                    :test #'equal))
+                    sum 1)))
 
