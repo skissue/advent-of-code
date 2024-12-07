6d0c0d9a9e3e6e6eefff47b2d3c8b9bddbb01621
Ad <me@skissue.xyz>
2024-12-07 08:36:52 -0500

    day 7: solve part 1 and 2
    Forgot to commit just part 1 💀.

---
+++ day7.lisp
@@ -0,0 +1,40 @@
+(ql:quickload :cl-ppcre)
+
+(defun parse-data (path)
+  (mapcar (lambda (x)
+            (mapcar #'parse-integer
+                    (cl-ppcre:split ":? " x)))  
+          (uiop:read-file-lines path)))
+
+(defparameter *sample-data* (parse-data "sample/day7.txt"))
+(defparameter *data* (parse-data "input/day7.txt"))
+
+(defun calc-valid-p (target cur remaining)
+  (if (null remaining)
+      (= cur target)
+      (destructuring-bind (x . xs) remaining
+        (or (calc-valid-p target (+ cur x) xs)
+            (calc-valid-p target (* cur x) xs)))))
+
+(defun part1 (data)
+  (loop for (target . nums) in data
+        when (calc-valid-p target 0 nums)
+          sum target))
+
+(defun concat (a b)
+  (declare (optimize (speed 3)))
+  (let ((digits (1+ (floor (log b 10)))))
+    (+ (* a (expt 10 digits)) b)))
+
+(defun calc-valid-p-2 (target cur remaining)
+  (if (null remaining)
+      (= cur target)
+      (destructuring-bind (x . xs) remaining
+        (or (calc-valid-p-2 target (+ cur x) xs)
+            (calc-valid-p-2 target (* cur x) xs)
+            (calc-valid-p-2 target (concat cur x) xs)))))
+
+(defun part2 (data)
+  (loop for (target . (start . nums)) in data
+        when (calc-valid-p-2 target start nums)
+          sum target))
