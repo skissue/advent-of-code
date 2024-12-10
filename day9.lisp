c39caf4117e3857d6c82f1af5c61d79e0dce805c
Ad <me@skissue.xyz>
2024-12-09 20:25:28 -0500

    day 9: solve part 1
    One day behind 😔.

--- day9.lisp
+++ day9.lisp
@@ -23,3 +23,22 @@
 
 (defparameter *sample-data* (parse-data "sample/day9.txt"))
 (defparameter *data* (parse-data "input/day9.txt"))
+
+(defun part1 (data)
+  (loop with i = 0
+        with j = (1- (length data))
+        while (< i j)
+        if (aref data i) do
+          (incf i)
+        else
+          if (null (aref data j)) do
+            (decf j)
+        else do
+          (rotatef (aref data i) (aref data j))
+          (incf i)
+          (decf j))
+  (loop for i from 0
+        for x across data
+        when x
+          sum (* i x)))
+
